ba284d065e0ff51b246f0ab220c91dbcd7f6795f
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-06-10 22:34:47 -0700

    Support square brackets

---
+++ AssetRipper.Text.SourceGeneration/SquareBrackets.cs
@@ -0,0 +1,20 @@
+﻿namespace AssetRipper.Text.SourceGeneration;
+
+public readonly ref struct SquareBrackets
+{
+	private readonly Indented indented;
+
+	public IndentedTextWriter Writer => indented.Writer;
+
+	public SquareBrackets(IndentedTextWriter writer)
+	{
+		writer.WriteLine('[');
+		indented = new Indented(writer);
+	}
+
+	public void Dispose()
+	{
+		indented.Dispose();
+		Writer.WriteLine(']');
+	}
+}
---
+++ AssetRipper.Text.SourceGeneration/SquareBracketsWithSemicolon.cs
@@ -0,0 +1,20 @@
+﻿namespace AssetRipper.Text.SourceGeneration;
+
+public readonly ref struct SquareBracketsWithSemicolon
+{
+	private readonly Indented indented;
+
+	public IndentedTextWriter Writer => indented.Writer;
+
+	public SquareBracketsWithSemicolon(IndentedTextWriter writer)
+	{
+		writer.WriteLine('[');
+		indented = new Indented(writer);
+	}
+
+	public void Dispose()
+	{
+		indented.Dispose();
+		Writer.WriteLine("];");
+	}
+}
