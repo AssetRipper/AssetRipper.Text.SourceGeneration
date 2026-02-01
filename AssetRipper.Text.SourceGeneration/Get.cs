65219667e78efdad0b05330c1bd80feb633f0a58
ds5678 <49847914+ds5678@users.noreply.github.com>
2026-02-01 13:53:53 -0800

    Add support for get and set property accessors

---
+++ AssetRipper.Text.SourceGeneration/Get.cs
@@ -0,0 +1,18 @@
+﻿namespace AssetRipper.Text.SourceGeneration;
+
+public readonly ref struct Get
+{
+	private readonly NamedBlock block;
+
+	public IndentedTextWriter Writer => block.Writer;
+
+	public Get(IndentedTextWriter writer)
+	{
+		block = new NamedBlock(writer, "get");
+	}
+
+	public void Dispose()
+	{
+		block.Dispose();
+	}
+}
---
+++ AssetRipper.Text.SourceGeneration/Set.cs
@@ -0,0 +1,18 @@
+﻿namespace AssetRipper.Text.SourceGeneration;
+
+public readonly ref struct Set
+{
+	private readonly NamedBlock block;
+
+	public IndentedTextWriter Writer => block.Writer;
+
+	public Set(IndentedTextWriter writer)
+	{
+		block = new NamedBlock(writer, "set");
+	}
+
+	public void Dispose()
+	{
+		block.Dispose();
+	}
+}
